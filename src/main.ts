8f2818b5034409a3b6e81e6429e484a29bf8853a
kurkin <deemson.k@gmail.com>
2022-11-13 21:57:34 +0200

    upd initialization, added playground scene

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import * as Phaser from 'phaser'
+import { PlaygroundScene } from './scene'
 
 new Phaser.Game({
+  parent: 'phaser-playground',
   type: Phaser.WEBGL,
   width: 800,
-  height: 600
+  height: 600,
+  scene: new PlaygroundScene({}),
 })
