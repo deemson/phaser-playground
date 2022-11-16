d4954ba12ec2130317904743e5799053ec5da0a5
kurkin <deemson.k@gmail.com>
2022-11-16 23:01:35 +0200

    resizing window

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,25 @@
 import * as Phaser from 'phaser'
 
+type PhaserSize = Phaser.Structs.Size
+
 class PlaygroundScene extends Phaser.Scene {
+  private label: Phaser.GameObjects.Text
+
   create () {
+    const { width, height } = this.scale.gameSize
+    this.label = this.add.text(0, 0, `${width}x${height}`)
+    const x = width / 2 - this.label.width / 2
+    const y = height / 2 - this.label.height / 2
+    this.label.setPosition(x, y)
+    this.scale.on('resize', this.resize, this)
+  }
+
+  resize (gameSize: PhaserSize, baseSize: PhaserSize, displaySize: PhaserSize, width: number, height: number) {
+    this.cameras.resize(gameSize.width, gameSize.height)
+    const x = gameSize.width / 2 - this.label.width / 2
+    const y = gameSize.height / 2 - this.label.height / 2
+    this.label.setText(`${gameSize.width}x${gameSize.height}`)
+    this.label.setPosition(x, y)
   }
 }
 
@@ -11,7 +29,7 @@ new Phaser.Game({
   width: 800,
   height: 600,
   scale: {
-    mode: Phaser.Scale.ScaleModes.FIT
+    mode: Phaser.Scale.ScaleModes.RESIZE
   },
   scene: new PlaygroundScene({})
 })
